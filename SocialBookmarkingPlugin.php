omeka/plugin-SocialBookmarking

commit cf6b7813299c09b20c7c45e877293930078291a3

Remove "Social Bookmarking" headings

SocialBookmarkingPlugin.php
@@ -129,9 +129,8 @@ class SocialBookmarkingPlugin extends Omeka_Plugin_AbstractPlugin
         if (get_option(SocialBookmarkingPlugin::ADD_TO_OMEKA_ITEMS_OPTION) == '1') {
             $item = get_current_record('item');
             $url = record_url($item, 'show', true);
-            $title = strip_formatting(metadata($item, array('Dublin Core', 'Title')));
-            $description = strip_formatting(metadata($item, array('Dublin Core', 'Description')));
-            echo '<h2>' . __('Social Bookmarking') . '</h2>';
+            $title = strip_formatting(metadata($item, array('Dublin Core', 'Title'), array('no_escape' => true)));
+            $description = strip_formatting(metadata($item, array('Dublin Core', 'Description'), array('no_escape' => true)));
             echo social_bookmarking_toolbar($url, $title, $description);
         }
     }
@@ -141,9 +140,8 @@ class SocialBookmarkingPlugin extends Omeka_Plugin_AbstractPlugin
         if (get_option(SocialBookmarkingPlugin::ADD_TO_OMEKA_COLLECTIONS_OPTION) == '1') {
             $collection = get_current_record('collection');
             $url = record_url($collection, 'show', true);
-            $title = strip_formatting(metadata($collection, array('Dublin Core', 'Title')));
-            $description = strip_formatting(metadata($collection, array('Dublin Core', 'Description')));
-            echo '<h2>' . __('Social Bookmarking') . '</h2>';
+            $title = strip_formatting(metadata($item, array('Dublin Core', 'Title'), array('no_escape' => true)));
+            $description = strip_formatting(metadata($item, array('Dublin Core', 'Description'), array('no_escape' => true)));
             echo social_bookmarking_toolbar($url, $title, $description);
         }
     }

commit 87f6a22952a23349d1b42a4840bacd278b9665c8

Fix display for collections
(broken in cf6b7813299c09b20c7c45e877293930078291a3)

SocialBookmarkingPlugin.php
@@ -141,8 +141,8 @@ class SocialBookmarkingPlugin extends Omeka_Plugin_AbstractPlugin
         if (get_option(SocialBookmarkingPlugin::ADD_TO_OMEKA_COLLECTIONS_OPTION) == '1') {
             $collection = get_current_record('collection');
             $url = record_url($collection, 'show', true);
-            $title = strip_formatting(metadata($item, array('Dublin Core', 'Title'), array('no_escape' => true)));
-            $description = strip_formatting(metadata($item, array('Dublin Core', 'Description'), array('no_escape' => true)));
+            $title = strip_formatting(metadata($collection, array('Dublin Core', 'Title'), array('no_escape' => true)));
+            $description = strip_formatting(metadata($collection, array('Dublin Core', 'Description'), array('no_escape' => true)));
             echo social_bookmarking_toolbar($url, $title, $description);
         }
     }