e3f77aee9b80743ac8cd7762a06998bf05e360c0
John Flatness <john@zerocrates.org>
2023-05-25 16:26:52 -0400

    Use description-first order as normal for admin

--- config_form.php
+++ config_form.php
@@ -4,13 +4,13 @@
             <?php echo get_view()->formLabel(SocialBookmarkingPlugin::ADD_TO_OMEKA_ITEMS_OPTION, 'Add to Items'); ?>
         </div>
         <div class="inputs five columns omega">
+            <p class="explanation"><?php echo __(
+                'If checked, this plugin will add a social bookmarking toolbar at the bottom of every public item show page.'
+            ); ?></p>
             <?php echo get_view()->formCheckbox(
                 SocialBookmarkingPlugin::ADD_TO_OMEKA_ITEMS_OPTION,
                 true,
                 array('checked' => (boolean) get_option(SocialBookmarkingPlugin::ADD_TO_OMEKA_ITEMS_OPTION))); ?>
-            <p class="explanation"><?php echo __(
-                'If checked, this plugin will add a social bookmarking toolbar at the bottom of every public item show page.'
-            ); ?></p>
         </div>
     </div>
 
@@ -19,13 +19,13 @@
             <?php echo get_view()->formLabel(SocialBookmarkingPlugin::ADD_TO_OMEKA_COLLECTIONS_OPTION, 'Add to Collections'); ?>
         </div>
         <div class="inputs five columns omega">
+            <p class="explanation"><?php echo __(
+                'If checked, this plugin will add a social bookmarking toolbar at the bottom of every public collection show page.'
+            ); ?></p>
             <?php echo get_view()->formCheckbox(
                 SocialBookmarkingPlugin::ADD_TO_OMEKA_COLLECTIONS_OPTION,
                 true,
                 array('checked' => (boolean) get_option(SocialBookmarkingPlugin::ADD_TO_OMEKA_COLLECTIONS_OPTION))); ?>
-            <p class="explanation"><?php echo __(
-                'If checked, this plugin will add a social bookmarking toolbar at the bottom of every public collection show page.'
-            ); ?></p>
         </div>
     </div>
 
